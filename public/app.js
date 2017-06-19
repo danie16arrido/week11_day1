6055dea9e96a05322feaa2760f0fc8a799990691
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-19 21:40:51 +0100

    remove data from html file to an array in app.js

--- public/app.js
+++ public/app.js
@@ -1,11 +1,25 @@
-var data = {
-  catName: "Lucipurr",
-  catsFood: "Dog's heart",
-  catsImg: "https://img.wikinut.com/img/19cgiwd67cvbziym/jpeg/0/Satanic-Cat.jpeg"
-}
-var app = function () {
-  addCatToList( data.catName, data.catsFood, data.catsImg ); 
-};
+var catabase = [
+  {
+  name: "Lucipurr",
+  favFood: "Dog's heart",
+  img: "https://img.wikinut.com/img/19cgiwd67cvbziym/jpeg/0/Satanic-Cat.jpeg"
+  },
+  {
+  name: "Boba",
+  favFood: "Sock fluff",
+  img: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"
+  },
+  {
+  name: "Barnaby",
+  favFood: "Tuna",
+  img: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"
+  },
+  {
+  name: "Max",
+  favFood: "Whiskas Temptations",
+  img: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"
+  }
+];
 
 var addCatToList = function( catName, catsFood, catsImg ) {
   var ulElement = createUlclass( 'cat' );
@@ -45,8 +59,13 @@ var appendElements = function( catClass, liNodes ) {
   list.appendChild( catClass );
 }
 
-window.onload = app;
+var app = function () {
+  for(var cat of catabase){
+    addCatToList( cat.name, cat.favFood, cat.img );
+  }
+};
 
+window.onload = app;
 
 
 
